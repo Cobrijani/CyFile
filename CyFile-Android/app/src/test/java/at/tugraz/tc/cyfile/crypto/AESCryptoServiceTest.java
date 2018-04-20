74ebc9eb8b799c7bcf85936b61462554a1c6e59d
Maximilian Baronig <baronig@student.tugraz.at>
2018-04-20 16:27:04 +0200

    added Cypher Tests

--- CyFile-Android/app/src/test/java/at/tugraz/tc/cyfile/crypto/AESCryptoServiceTest.java
+++ CyFile-Android/app/src/test/java/at/tugraz/tc/cyfile/crypto/AESCryptoServiceTest.java
@@ -18,6 +18,9 @@ public class AESCryptoServiceTest {
         cryptoService = new AESCryptoService();
     }
 
+    //TODO: find out Block Size
+    private int blockSize = 32;
+
 
     @Test
     public void testEncryptDecryptString () {
@@ -29,4 +32,53 @@ public class AESCryptoServiceTest {
         assertEquals(plain, decrypted);
     }
 
+    @Test
+    public void testEncryptEmptyString () {
+        String plain = "";
+        String encrypted = cryptoService.encrypt(plain);
+        assertNotSame(plain, encrypted);
+        assertTrue(encrypted.length() > 0);
+        String decrypted = cryptoService.decrypt(encrypted);
+        assertEquals(plain, decrypted);
+    }
+
+    @Test
+    public void testEncryptNotBlockSizeAlignedString () {
+        //TODO: Find out Block Size for Block Size unaligned String
+        String plain = "Hello this is a not Block-Size aligned String!";
+        assertNotSame(0, plain.length() % blockSize);
+        String encrypted = cryptoService.encrypt(plain);
+        assertNotSame(plain, encrypted);
+        assertTrue(encrypted.length() > 0);
+        String decrypted = cryptoService.decrypt(encrypted);
+        assertEquals(plain, decrypted);
+    }
+
+    @Test
+    public void testEncryptBlockSizeAlignedString () {
+        //TODO: Find out Block Size for Block Size aligned String
+        String plain = "This is gonna be a Block-Size aligned String, achieved by cropping!" +
+                " In order to fit different block sizes, this has to be a quite large String." +
+                " If it is too short, just append something at the end.";
+        assertTrue(plain.length() >= blockSize);
+        plain = plain.substring(0, blockSize);
+        assertEquals(0, plain.length() % blockSize);
+        String encrypted = cryptoService.encrypt(plain);
+        assertNotSame(plain, encrypted);
+        assertTrue(encrypted.length() > 0);
+        String decrypted = cryptoService.decrypt(encrypted);
+        assertEquals(plain, decrypted);
+    }
+
+    @Test
+    public void testEncryptSameStringMultipleTimes () {
+        String plain = "Hello World!";
+        String encrypted1 = cryptoService.encrypt(plain);
+        assertNotSame(plain, encrypted1);
+        String encrypted2 = cryptoService.encrypt(plain);
+        assertNotSame(plain, encrypted2);
+        assertTrue(encrypted1.length() > 0);
+        assertTrue(encrypted2.length() > 0);
+        assertTrue(!encrypted1.equals(encrypted2));
+    }
 }
