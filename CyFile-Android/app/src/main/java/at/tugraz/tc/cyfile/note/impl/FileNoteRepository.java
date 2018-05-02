24c6bd0ccda8fcedfb6814a6023071d3dfee8b28
David Froehlich <david.froehlich@student.tugraz.at>
2018-05-02 09:34:48 +0200

    [DF,MM] added stub for FileNoteRepository

---
+++ CyFile-Android/app/src/main/java/at/tugraz/tc/cyfile/note/impl/FileNoteRepository.java
@@ -0,0 +1,28 @@
+package at.tugraz.tc.cyfile.note.impl;
+
+import java.util.List;
+
+import at.tugraz.tc.cyfile.domain.Note;
+import at.tugraz.tc.cyfile.note.NoteRepository;
+
+public class FileNoteRepository implements NoteRepository {
+    @Override
+    public List<Note> findAll() {
+        return null;
+    }
+
+    @Override
+    public Note findOne(String id) {
+        return null;
+    }
+
+    @Override
+    public Note save(Note note) {
+        return null;
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+}
