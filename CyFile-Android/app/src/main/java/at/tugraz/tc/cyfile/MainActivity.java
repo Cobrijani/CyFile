6b5bd4bedd13b3d57e8cb820b00683b0ef4fbebc
cHgcH <max.baronig@gmail.com>
2018-05-06 16:41:23 +0200

    [MB, AT] First implementation of new List View

--- CyFile-Android/app/src/main/java/at/tugraz/tc/cyfile/MainActivity.java
+++ CyFile-Android/app/src/main/java/at/tugraz/tc/cyfile/MainActivity.java
@@ -3,10 +3,15 @@ package at.tugraz.tc.cyfile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -15,6 +20,8 @@ import at.tugraz.tc.cyfile.note.NoteService;
 import at.tugraz.tc.cyfile.secret.SecretPrompter;
 import at.tugraz.tc.cyfile.ui.BaseActivity;
 import at.tugraz.tc.cyfile.ui.DisplayNoteActivity;
+import at.tugraz.tc.cyfile.ui.NotesAdapter;
+import co.dift.ui.SwipeToAction;
 
 
 /**
@@ -29,6 +36,12 @@ public class MainActivity extends BaseActivity {
     @Inject
     SecretPrompter secretPrompter;
 
+    RecyclerView recyclerView;
+    NotesAdapter adapter;
+    SwipeToAction swipeToAction;
+
+    List<Note> notes = new ArrayList<>();
+
     public static final String NOTE_ID = "NOTE_ID";
 
     @Override
@@ -37,6 +50,34 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         getActivityComponent().inject(this);
         secretPrompter.promptSecret();
+            recyclerView = (RecyclerView) findViewById(R.id.noteList);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setHasFixedSize(true);
+
+            adapter = new NotesAdapter(this.notes);
+            recyclerView.setAdapter(adapter);
+
+            swipeToAction = new SwipeToAction(recyclerView, new SwipeToAction.SwipeListener<Note>() {
+                @Override
+                public boolean swipeLeft(final Note itemData) {
+
+                    return true;
+                }
+
+                @Override
+                public boolean swipeRight(Note itemData) {
+                    return true;
+                }
+
+                @Override
+                public void onClick(Note itemData) {
+                }
+
+                @Override
+                public void onLongClick(Note itemData) {
+                }
+            });
     }
 
     @Override
@@ -47,11 +88,12 @@ public class MainActivity extends BaseActivity {
     }
 
     private void loadNoteList() {
-        final LinearLayout linear = findViewById(R.id.noteList);
+        final RecyclerView linear = findViewById(R.id.noteList);
         linear.removeAllViews();
 
         for (Note note :
                 noteService.findAll()) {
+            /*
             Button btn = new Button(this);
             btn.setTextSize(11);
             btn.setText(note.getTitle());
@@ -59,7 +101,8 @@ public class MainActivity extends BaseActivity {
             btn.setWidth(linear.getWidth());
             btn.setTag(note.getId());
             btn.setOnClickListener(view -> openNoteInDetailActivity(note.getId()));
-            linear.addView(btn);
+            linear.addView(btn);*/
+            this.notes.add(note);
         }
     }
 
@@ -81,6 +124,4 @@ public class MainActivity extends BaseActivity {
         Intent intent = new Intent(this, DisplayNoteActivity.class);
         startActivity(intent);
     }
-
-    ;
 }
