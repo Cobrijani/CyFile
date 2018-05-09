f8d22d66bb17d5da341561cdba0b35e3105ae1a3
Anil Kumar Tilanthe <aniltilanthe92@gmail.com>
2018-05-09 11:29:19 +0200

    [MB, AT] left swipe delete action and confirmation

--- CyFile-Android/app/src/main/java/at/tugraz/tc/cyfile/MainActivity.java
+++ CyFile-Android/app/src/main/java/at/tugraz/tc/cyfile/MainActivity.java
@@ -1,14 +1,17 @@
 package at.tugraz.tc.cyfile;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -64,13 +67,22 @@ public class MainActivity extends BaseActivity {
         swipeToAction = new SwipeToAction(recyclerView, new SwipeToAction.SwipeListener<Note>() {
             @Override
             public boolean swipeLeft(final Note itemData) {
+                displaySnackbar("remove " + itemData.getTitle() + "?", "Confirm", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onSelectDeleteNote(itemData.getId());
+                        updateNoteList();
+                    }
+                });
                 return true;
             }
 
             @Override
             public boolean swipeRight(Note itemData) {
+                openNoteInDetailActivity(itemData.getId());
                 return true;
             }
+
 
             @Override
             public void onClick(Note itemData) {
@@ -113,4 +125,27 @@ public class MainActivity extends BaseActivity {
         Intent intent = new Intent(this, DisplayNoteActivity.class);
         startActivity(intent);
     }
+
+    public void onSelectDeleteNote(String noteId) {
+        Log.d("onSelectDeleteNote", "on select delete note: " + noteId);
+
+
+
+        noteService.delete(noteId);
+
+    }
+
+    private void displaySnackbar(String text, String actionName, View.OnClickListener action) {
+        Snackbar snack = Snackbar.make(findViewById(android.R.id.content), text, Snackbar.LENGTH_LONG)
+                .setAction(actionName, action);
+
+        View v = snack.getView();
+        v.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        ((TextView) v.findViewById(android.support.design.R.id.snackbar_text)).setTextColor(Color.WHITE);
+        ((TextView) v.findViewById(android.support.design.R.id.snackbar_action))
+                .setTextColor(getResources().getColor(R.color.colorDeleteButton));
+
+        snack.show();
+    }
+
 }
