5a33c05e6f4f2f3579003a4dbc739000b941c345
Anil Kumar Tilanthe <aniltilanthe92@gmail.com>
2018-06-04 19:59:02 +0200

    [DF, AT] request permission settings

--- CyFile-Android/app/src/main/java/at/tugraz/tc/cyfile/ui/SettingsActivity.java
+++ CyFile-Android/app/src/main/java/at/tugraz/tc/cyfile/ui/SettingsActivity.java
@@ -1,8 +1,14 @@
 package at.tugraz.tc.cyfile.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +28,7 @@ public class SettingsActivity extends BaseActivity {
     private Switch stealthModeSwitch = null;
     private EditText phoneNmbrEditText = null;
 
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
 
     @Inject
     HidingComponent hidingComponent;
@@ -49,17 +56,59 @@ public class SettingsActivity extends BaseActivity {
 
         stealthModeSwitch = findViewById(R.id.hide_app_switch);
         stealthModeSwitch.setChecked(userSettings.isStealthMode());
+
         stealthModeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
-                hidingComponent.hideApp(this);
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.PROCESS_OUTGOING_CALLS)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    hidingComponent.hideApp(this);
+
+                } else if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.PROCESS_OUTGOING_CALLS)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Permission is not granted
+                    // No explanation needed; request the permission
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.PROCESS_OUTGOING_CALLS},
+                            MY_PERMISSIONS_REQUEST_CALL_PHONE);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
             } else {
                 hidingComponent.showApp(this);
             }
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CALL_PHONE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    hidingComponent.hideApp(this);
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
     private void saveSettings() {
-        boolean stealthMode = this.stealthModeSwitch.isActivated();
+        boolean stealthMode = this.stealthModeSwitch.isChecked();
         String magicPhoneNumber = this.phoneNmbrEditText.getText().toString();
         boolean numberIsOk = magicPhoneNumber.length() > 1;
         try {
